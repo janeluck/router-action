f34583c3dbdabfb24ac205f1d03d4baef5d12d32
janeluck <janeluck158@gmail.com>
2018-04-24 06:50:08 +0800

    ok

---
+++ src/modules/jRouter.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jane on 24/04/2018.
+ *
+ * 从零写一个前端路由
+ * 理解history, react-router设计思想和源码
+ */
+
+
+function createHistory() {
+
+
+
+
+
+
+    return {
+        length,
+        location,
+        listen,
+    } = {}
+}
