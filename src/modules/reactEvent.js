fc262fb184e63538f568ea2ee2df08af04153a20
janeluck <janeluck158@gmail.com>
2018-05-07 20:16:07 +0800

    ok

--- src/modules/reactEvent.js
+++ src/modules/reactEvent.js
@@ -7,8 +7,14 @@ export default class A extends Component {
 
     componentDidMount() {
         this.div.addEventListener('click', e => {
+
             console.log('dom event')
-        })
+        }, false)
+
+        this.div.addEventListener('click', e => {
+
+            console.log('dom event  capture')
+        }, true)
     }
 
     componentWillUnMount() {
@@ -17,18 +23,24 @@ export default class A extends Component {
 
     divClick = (e) => {
         console.log('react event')
-        e.stopPropagation()
+
     }
 
     render() {
         return <div>
             click
             <div>
-                <div ref={div => this.div = div} onClickCapture={this.divClick}>
+                <div ref={div => this.div = div} onClick={this.divClick}>
 
-                    <div onClickCapture={() => {
+                    <div onClick={() => {
                         console.log('div child event click')
-                    }}>
+                    }}
+
+                         onClickCapture={() => {
+                             console.log('11div child event click')
+                         }}
+
+                    >
                         divchild
                     </div>
 
