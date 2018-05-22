6a034765f8adaa77b1ae078f8be658533935c15f
janeluck <janeluck158@gmail.com>
2018-05-23 07:20:30 +0800

    ok

---
+++ src/wheel/AReact.js
@@ -0,0 +1,27 @@
+/**
+ * Created by jane on 03/05/2018.
+ *
+ * 从零写一个react
+ *
+ * + Element
+ *      - VDOM object
+ *      - jsx
+ *
+ * + Rendering
+ *
+ *
+ * + Patching
+ *
+ *
+ * + Component
+ *      - lifecycle
+ *
+ *
+ */
+
+
+
+
+
+
+
